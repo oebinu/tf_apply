21234b5b814257aacbb99a76ab85e71657c4f7fe
oebinu <oebinu2021@gmail.com>
2022-07-31 16:13:23 +0900

    Update EKS(2).tf

--- eks.tf
+++ eks.tf
@@ -3,7 +3,7 @@
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.this.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority.0.data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
+  token                  = data.aws_eks_cluster_auth.this.token
 }
 
 provider "helm" {
