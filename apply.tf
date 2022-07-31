e29e7a1a5673d890e1642d1d3af611602f64267e
oebinu <oebinu2021@gmail.com>
2022-07-31 13:42:24 +0900

    Update var.tf

--- apply.tf
+++ apply.tf
@@ -7,7 +7,7 @@ provider "aws" {
 locals {
   name = basename(path.cwd)
   # var.cluster_name is for Terratest
-#   cluster_name = coalesce(var.cluster_name, local.name)
+  cluster_name = coalesce(var.cluster_name, local.name)
   region       = "ap-northeast-2"
 
   vpc_cidr = "200.0.0.0/16"
