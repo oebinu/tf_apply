d9b777a73f6c1ecc40625275db4f65606cdafc0a
oebinu <oebinu2021@gmail.com>
2022-07-31 16:05:32 +0900

    Update EKS.tf

--- vpc.tf
+++ vpc.tf
@@ -4,20 +4,6 @@ provider "aws" {
   region = local.region
 }
 
-provider "kubernetes" {
-  host                   = module.eks_blueprints.eks_cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks_blueprints.eks_cluster_certificate_authority_data)
-  token                  = data.aws_eks_cluster_auth.this.token
-}
-
-provider "helm" {
-  kubernetes {
-    host                   = module.eks_blueprints.eks_cluster_endpoint
-    cluster_ca_certificate = base64decode(module.eks_blueprints.eks_cluster_certificate_authority_data)
-    token                  = data.aws_eks_cluster_auth.this.token
-  }
-}
-
 # terraform {
 #   required_version = ">= 0.13.1"
 
