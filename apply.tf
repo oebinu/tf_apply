5602c9a3e0e91c5ea94e704ec9aaab0933c39f41
oebinu <oebinu2021@gmail.com>
2022-07-31 14:11:49 +0900

    Upload variable.tf

--- apply.tf
+++ apply.tf
@@ -4,16 +4,16 @@ provider "aws" {
   region = local.region
 }
 
-terraform {
-  required_version = ">= 0.13.1"
+# terraform {
+#   required_version = ">= 0.13.1"
 
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = ">= 3.63.0"
-    }
-  }
-}
+#   required_providers {
+#     aws = {
+#       source = "hashicorp/aws"
+#       version = ">= 3.63.0"
+#     }
+#   }
+# }
 
 locals {
   name = basename(path.cwd)
